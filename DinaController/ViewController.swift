e3b1b5dc351bbccad65556f1e26df9369e399093
Luka Gabrić <luka@medable.com>
2020-08-13 23:07:26 +0200

    fine tune joystick speed mapping

--- DinaController/ViewController.swift
+++ DinaController/ViewController.swift
@@ -52,47 +52,70 @@ class ViewController: UIViewController {
         joystick.monitor = .polar(monitor: { [weak self] report in
             guard let self = self else { return }
             
-            let angle = Float(report.angle)
-            var displacement = Float(report.displacement)
+            let angle = Double(report.angle)
+            var displacement = Double(report.displacement)
                         
-            var leftWheelSpeed: Float = 0
-            var rightWheelSpeed: Float = 0
+            var leftWheelSpeed: Double = 0
+            var rightWheelSpeed: Double = 0
+            
+            let verticalAngleOffset = 20.0
             
             if angle >= 270 && angle < 360 {
-                leftWheelSpeed = self.map(x: angle, in_min: 270, in_max: 360, out_min: 0, out_max: 1)
+                if angle >= 360 - verticalAngleOffset {
+                    leftWheelSpeed = 1
+                } else {
+                    leftWheelSpeed = self.map(x: angle, in_min: 270, in_max: 360, out_min: 0, out_max: 1)
+                }
                 rightWheelSpeed = 1
             } else if angle >= 0 && angle <= 90 {
+                if angle <= verticalAngleOffset {
+                    rightWheelSpeed = 1
+                } else {
+                    rightWheelSpeed = self.map(x: angle, in_min: 0, in_max: 90, out_min: 1, out_max: 0)
+                }
                 leftWheelSpeed = 1
-                rightWheelSpeed = self.map(x: angle, in_min: 0, in_max: 90, out_min: 1, out_max: 0)
             } else if angle > 90 && angle <= 180 {
+                if angle >= 180 - verticalAngleOffset {
+                    rightWheelSpeed = -1
+                } else {
+                    rightWheelSpeed = self.map(x: angle, in_min: 90, in_max: 180, out_min: 0, out_max: -1)
+                }
                 leftWheelSpeed = -1
-                rightWheelSpeed = self.map(x: angle, in_min: 90, in_max: 180, out_min: 0, out_max: -1)
             } else if angle > 180 && angle <= 270 {
-                leftWheelSpeed = self.map(x: angle, in_min: 180, in_max: 270, out_min: -1, out_max: 0)
+                if angle <= 200 {
+                    leftWheelSpeed = -1
+                } else {
+                    leftWheelSpeed = self.map(x: angle, in_min: 180, in_max: 270, out_min: -1, out_max: 0)
+                }
                 rightWheelSpeed = -1
             }
             
             if displacement < 0.2 {
                 displacement = 0
+            } else if displacement > 0.8 {
+                displacement = 1
             }
                         
-            if leftWheelSpeed > 0.9 {
+            if leftWheelSpeed > 0.8 {
                 leftWheelSpeed = 1
-            } else if leftWheelSpeed < -0.9 {
+            } else if leftWheelSpeed < -0.8 {
                 leftWheelSpeed = -1
             }
 
-            if rightWheelSpeed > 0.9 {
+            if rightWheelSpeed > 0.8 {
                 rightWheelSpeed = 1
-            } else if rightWheelSpeed < -0.9 {
+            } else if rightWheelSpeed < -0.8 {
                 rightWheelSpeed = -1
             }
+            
+            let lws = Int(round(leftWheelSpeed * displacement * 100))
+            let rws = Int(round(rightWheelSpeed * displacement * 100))
 
-            self.currentJoystickCommand = "0,\(Int(leftWheelSpeed * displacement * 100)),\(Int(rightWheelSpeed * displacement * 100)),"
+            self.currentJoystickCommand = "0,\(lws),\(rws),"
         })
     }
     
-    private func map(x: Float, in_min: Float, in_max: Float, out_min: Float, out_max: Float) -> Float {
+    private func map(x: Double, in_min: Double, in_max: Double, out_min: Double, out_max: Double) -> Double {
         (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
     }
     
